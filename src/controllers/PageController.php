04ebdb70a6ac39ad6b39caf02a0885a4758f666b
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-26 11:29:42 +0300

    Testing

--- src/controllers/PageController.php
+++ src/controllers/PageController.php
@@ -29,7 +29,6 @@ class PageController extends BaseController
     	}
 
     	if (strlen($browser_title) == 0){
-    		header("HTTP/1.0 404 Not Found");
     		header("Location:/page-not-found");
     		exit();
     	}
@@ -42,6 +41,7 @@ class PageController extends BaseController
 
     public function getShow404()
     {
+        header("HTTP/1.0 404 Not Found");
     	echo $this->blade->render('page-not-found');
     }
 
